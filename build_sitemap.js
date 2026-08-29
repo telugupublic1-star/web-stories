2a28a8e4066042dc57b837ed1ab9892dc5d2ee3e
Telugu Public <telugupublic1@gmail.com>
2026-08-29 09:30:29 +0530

    Remove create.html and revert header

--- build_sitemap.js
+++ build_sitemap.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storiesDir = __dirname;
 const files = fs.readdirSync(storiesDir)
-  .filter(f => f.endsWith('.html') && f !== 'index.html' && f !== 'create.html')
+  .filter(f => f.endsWith('.html') && f !== 'index.html')
   .sort((a, b) => {
     const statA = fs.statSync(path.join(storiesDir, a));
     const statB = fs.statSync(path.join(storiesDir, b));
